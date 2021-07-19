8839c87cd031584d7882d72a5bd977f83e4754b7
warmstarter <79870389+warmstarter@users.noreply.github.com>
2021-07-19 03:24:18 -0400

    Moved COPYING to LICENSE, removed redundanceies

--- src/output.h
+++ src/output.h
@@ -3,7 +3,7 @@
  *  Copyright (C) 1993-2007 Ken Keys (kenkeys@users.sourceforge.net)
  *
  *  TinyFugue (aka "tf") is protected under the terms of the GNU
- *  General Public License.  See the file "COPYING" for details.
+ *  General Public License.  See the file "LICENSE" for details.
  ************************************************************************/
 
 #ifndef OUTPUT_H
